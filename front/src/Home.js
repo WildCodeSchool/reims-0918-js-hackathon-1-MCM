e744883f92cf590f2b0fe7c37084cb5e865b6c68
Corentin DESOTOCOBET <corentin.dsc@gmail.com>
2018-10-30 13:16:20 +0100

    bootstrap + navtabs

---
+++ front/src/Home.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import {
+  TabContent,
+  TabPane,
+  Nav,
+  NavItem,
+  NavLink,
+  Row,
+  Col
+} from "reactstrap";
+import classnames from "classnames";
+
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeTab: "1"
+    };
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle(tab) {
+    if (this.state.activeTab !== tab) {
+      this.setState({
+        activeTab: tab
+      });
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Nav tabs className="justify-content-center">
+          <NavItem>
+            <NavLink
+              style={{ cursor: "pointer" }}
+              className={classnames("navlink", {
+                active: this.state.activeTab === "1"
+              })}
+              onClick={() => {
+                this.toggle("1");
+              }}
+            >
+              Adresses
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              style={{ cursor: "pointer" }}
+              className={classnames("navlink", {
+                active: this.state.activeTab === "1"
+              })}
+              onClick={() => {
+                this.toggle("2");
+              }}
+            >
+              BonbonsDex
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink
+              style={{ cursor: "pointer" }}
+              className={classnames("navlink", {
+                active: this.state.activeTab === "1"
+              })}
+              onClick={() => {
+                this.toggle("3");
+              }}
+            >
+              Historique
+            </NavLink>
+          </NavItem>
+        </Nav>
+        <TabContent activeTab={this.state.activeTab}>
+          <TabPane tabId="1">
+            <Row>
+              <Col sm="12">
+                <h4>Listes d'adresses</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="2">
+            <Row>
+              <Col sm="12">
+                <h4>BonbonsDex</h4>
+              </Col>
+            </Row>
+          </TabPane>
+          <TabPane tabId="3">
+            <Row>
+              <Col sm="12">
+                <h4>Historique de la quête bonbon</h4>
+              </Col>
+            </Row>
+          </TabPane>
+        </TabContent>
+      </div>
+    );
+  }
+}
+
+export default Home;
