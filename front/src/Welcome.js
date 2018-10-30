3e8dfbdd61ac3209b5bc2695e3b4dae7a3dd3493
Michel <descotesmichel@aol.com>
2018-10-30 18:15:30 +0100

    Welcome display

--- front/src/Welcome.js
+++ front/src/Welcome.js
@@ -1,31 +1,40 @@
 import React, { Component } from "react";
-
+import { Row, Col, Container } from "reactstrap";
 
 class Welcome extends Component {
   render() {
     return (
-      <form>
-        <label>
-          Name :
-          <input
-            id="name"
-            value={this.props.valeur}
-            onChange={this.props.handleChangeName}
-            type="text"
-          />
-        </label>
-        <br/>
-        <br/>
-        <label>
-          City :
-          <input
-            id="city"
-            value={this.props.valeur}
-            onChange={this.props.handleChangeCity}
-            type="text"
-          />
-        </label>
-      </form>
+      <Container>
+          <Row>
+          <Col xs="6">
+              <label>
+                Name :
+                <input
+                  id="name"
+                  value={this.props.valeur}
+                  onChange={this.props.handleChangeName}
+                  type="text"
+                />
+              </label>
+            </Col>
+
+            <Col xs="6">
+              <label>
+                City :
+                <input
+                  id="city"
+                  value={this.props.valeur}
+                  onChange={this.props.handleChangeCity}
+                  type="text"
+                />
+              </label>
+            </Col>
+
+            <Col>
+              <button onClick={""}>Ajouter 1</button>
+            </Col>
+          </Row>
+      </Container>
     );
   }
 }
